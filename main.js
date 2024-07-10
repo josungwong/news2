8d87384aa626a05a1fa359b46402226d20fdb45b
josungwong <jsw13160@gmail.com>
2024-07-10 16:11:07 +0900

    너무 어려우어엉

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const apiKey = `fa80bc1d486a4ac3b407ca0ee368b453`
-const COUNTRY = "kr"
+let COUNTRY = "us"
+let query = ""
+let topButton = document.querySelectorAll(".menus button")
 let newsList = []
+
+topButton.forEach((menu)=>
+    menu.addEventListener("click",(event)=>{
+        if(event){
+            query = event.target.id
+        }
+        console.log(query)
+        getLatestNews()
+    }))
 
 const getLatestNews = async()=>{ // async: 동기함수로 만들기 (await으로 기다리게 만들수 있음)
 
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=${COUNTRY}&apiKey=${apiKey}`) // URL: 인스턴스, 미리 필요한걸 해주는거
+    const url = new URL(`https://newsapi.org/v2/top-headlines?q=${query}&country=${COUNTRY}&apiKey=${apiKey}`) // URL: 인스턴스, 미리 필요한걸 해주는거
 
     console.log("wow",url)
 
@@ -20,16 +31,45 @@ const getLatestNews = async()=>{ // async: 동기함수로 만들기 (await으�
 const render = () =>{
     const newsHTML = newsList.map(news=>`<div class="row news">
         <div class="col-lg-4">
-        <img class="news-img-size" src=${news.urlToImage} alt="">
+        <img class="news-img-size" src=${news.urlToImage} onerror="this.onerror=null; this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUnvISVTYopMAy17o3mB2lfSPeEjoKfAdV2w&s';">
     </div>
     <div class="col-lg-8">
         <h2>${news.title}</h2>
-        <p>${news.description}</p>
-        <div>${news.source.name} * ${news.publishedAt}</div>
+        <p>${overTwoH(news)}</p>
+        <div>${noSource(news)} * ${coolTime(news)}</div>
     </div>
     </div>`).join('')
 
     document.getElementById('news-board').innerHTML = newsHTML
+}
+const overTwoH = (news)=> {
+    let arr = ""
+    arr = news.description
+    if(arr.length > 200){
+        arr = arr.substring(0,200) + "..."
+        return arr
+    } else if(arr.length <= 0){
+        arr = "내용없음"
+        return arr
+    } else {
+        return arr
+    }
+}
+const noSource= (news) =>{
+    let arr1 = ""
+    arr1 = news.source.name
+    if(arr1.length <= 0){
+        arr1 = "no source"
+        return arr1
+    } else{
+        return arr1
+    }
+} 
+const coolTime = (news) => {
+    let date = moment(news.publishedAt.substring(0,10),"YYYYMMDD").fromNow()
+    let hour = moment(news.publishedAt.substring(11,19),"h:mm:ss").fromNow()
+
+    return date +"  " + hour
 }
 getLatestNews()
 
